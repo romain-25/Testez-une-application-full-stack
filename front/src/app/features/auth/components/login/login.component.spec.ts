88381d907d3f7c2abfe34bde866909471ccadadd
romain-25 <romain.rouabah@gmail.com>
2024-06-21 10:10:04 +0200

    add test page login and register

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -11,15 +11,38 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import {AuthService} from "../../services/auth.service";
+import {of, throwError} from "rxjs";
+import {Router} from "@angular/router";
+import {SessionInformation} from "../../../../interfaces/sessionInformation.interface";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthService;
+  let router: Router;
+  let sessionService: SessionService;
+  let spy: any;
 
   beforeEach(async () => {
+    const authServiceMock = {
+      login: jest.fn()
+    };
+
+    const routerMock = {
+      navigate: jest.fn()
+    };
+
+    const sessionServiceMock = {
+      logIn: jest.fn()
+    };
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers:[
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: Router, useValue: routerMock },
+        { provide: SessionService, useValue: sessionServiceMock }
+      ],
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
@@ -33,10 +56,35 @@ describe('LoginComponent', () => {
       .compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
+    sessionService = TestBed.inject(SessionService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should call authService.login and navigate on successful login', () => {
+    const loginRequest = { email: 'test@example.com', password: 'password123' };
+    const sessionInformation: SessionInformation = {} as SessionInformation;
+
+    component.form.setValue(loginRequest);
+    jest.spyOn(authService, 'login').mockReturnValue(of(sessionInformation));
+
+    component.submit();
+
+    expect(authService.login).toHaveBeenCalledWith(loginRequest);
+    expect(sessionService.logIn).toHaveBeenCalledWith(sessionInformation);
+    expect(router.navigate).toHaveBeenCalledWith(['/sessions']);
+  });
+
+  it('should set onError to true on login error', () => {
+    const loginRequest = { email: 'test@example.com', password: 'password123' };
+
+    component.form.setValue(loginRequest);
+    jest.spyOn(authService, 'login').mockReturnValue(throwError(() => new Error('test')));
+
+    component.submit();
+
+    expect(authService.login).toHaveBeenCalledWith(loginRequest);
+    expect(component.onError).toBe(true);
   });
 });
--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -9,32 +9,94 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
+import {RegisterRequest} from "../../interfaces/registerRequest.interface";
+import {of, throwError} from "rxjs";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(async () => {
+    const authServiceMock = {
+      register: jest.fn()
+    }
+    const routerMock = {
+      navigate: jest.fn()
+    }
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
-        ReactiveFormsModule,  
+        ReactiveFormsModule,
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
         MatInputModule
+      ],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: Router, useValue: routerMock },
       ]
     })
       .compileComponents();
-
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should call authService.register with correct data on submit', () => {
+    const registerRequest: RegisterRequest = {
+      email: 'test@example.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: 'password123'
+    };
+    component.form.setValue(registerRequest)
+    jest.spyOn(authService, 'register').mockReturnValue(of(void 0));
+    component.submit();
+    expect(authService.register).toHaveBeenCalledWith(registerRequest);
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
+  });
+  it('should set onError to true on registration error', () => {
+    const registerRequest: RegisterRequest = {
+      email: 'test@example.com',
+      firstName: 'John',
+      lastName: 'Doe',
+      password: 'password123'
+    };
+    component.form.setValue(registerRequest);
+    jest.spyOn(authService, 'register').mockReturnValue(throwError(() => new Error('test')));
+    component.submit();
+
+    expect(authService.register).toHaveBeenCalledWith(registerRequest);
+    expect(component.onError).toBe(true);
+  });
+  it('should display validation errors for required fields', () => {
+    component.form.controls['email'].setValue('');
+    component.form.controls['firstName'].setValue('');
+    component.form.controls['lastName'].setValue('');
+    component.form.controls['password'].setValue('');
+
+    fixture.detectChanges();
+
+    const emailError = fixture.nativeElement.querySelector('.email-error');
+    const firstNameError = fixture.nativeElement.querySelector('.firstName-error');
+    const lastNameError = fixture.nativeElement.querySelector('.lastName-error');
+    const passwordError = fixture.nativeElement.querySelector('.password-error');
+
+    expect(emailError).toBeTruthy();
+    expect(firstNameError).toBeTruthy();
+    expect(lastNameError).toBeTruthy();
+    expect(passwordError).toBeTruthy();
+  })
 });
